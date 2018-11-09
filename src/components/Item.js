c291c32bfd6ce72528b7c392ba26a516eb79e208
yapjinai <jinai.jyap@gmail.com>
2018-11-09 10:43:08 -0500

    still working on dragging

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react'
 import '../assets/css/Item.css'
-import { Draggable } from '@shopify/draggable';
-
 
 class Item extends Component {
+
+
   render() {
     const item = this.props.item
 
     return (
-      <div
-        className="Item"
-        style={item.style}
-      >
-        <img
-          src={item.image}
-          alt={item.name}
-        />
-      </div>
+        <div
+          className="Item"
+          style={item.style}
+        >
+          <img
+            src={item.image}
+            alt={item.name}
+          />
+        </div>
     )
-  }
-
-  componentDidMount() {
-
-    const draggable = new Draggable(document.querySelectorAll('.Shrine'), {
-      draggable: '.Item'
-    });
-    draggable.on('drag:start', () => console.log('drag:start'));
-    draggable.on('drag:move', () => console.log('drag:move'));
-    draggable.on('drag:stop', () => console.log('drag:stop'));
-
   }
 }
 
