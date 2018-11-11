9a36afabc8172baa94d100faf7253268749ce7ae
Chris Stull <chris.stull@gmail.com>
2018-11-11 18:56:54 -0500

    Left and top navbars

---
+++ src/components/Leftbar.js
@@ -0,0 +1,22 @@
+import React, { Component,PureComponent } from 'react'
+import '../assets/css/Leftbar.css'
+
+export default class Leftbar extends Component {
+
+  state = {
+    shown: true
+  }
+
+  move = () => {
+    this.setState({shown: !this.state.shown})
+  }
+
+  render(){
+    if(this.state.shown){
+      return <div className="leftbar" onClick={this.move}></div>
+      }
+    else {
+      return <div className="hidden-leftbar" onClick={this.move}></div>
+      }
+  }
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component,PureComponent } from 'react'
+import '../assets/css/Navbar.css'
+
+const Navbar = () => {
+
+  return(
+    <div className="navbar"></div>
+  )
+}
+
+export default Navbar
