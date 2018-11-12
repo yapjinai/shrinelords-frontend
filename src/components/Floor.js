470edd0e74763435df695bfb9cf12ffe8a07736b
yapjinai <jinai.jyap@gmail.com>
2018-11-12 13:24:10 -0500

    added files aaaah

---
+++ src/components/Floor.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../assets/css/Floor.css'
+
+class Floor extends Component {
+  render() {
+    const videoURL = './assets/video/sparkling_ocean_waves.mp4'
+
+    return (
+      <div className='Floor'>
+        <img
+          src='http://localhost:3001/assets/img/marble.jpg'
+        />
+      </div>
+    );
+  }
+}
+
+export default Floor;
