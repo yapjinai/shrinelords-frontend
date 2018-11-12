b26fdd60ce420bfbf3e003d7daa089f699e453ff
yapjinai <jinai.jyap@gmail.com>
2018-11-12 16:59:50 -0500

    refactor to add nested components

--- src/components/Editbar.js
+++ src/components/Editbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../assets/css/Editbar.css'
-import EditbarItem from './EditbarItem'
-// import Toolbar from './Toolbar'
+import Itembar from './Itembar'
+import Toolbar from './Toolbar'
 
 export default class Editbar extends Component {
 
@@ -16,8 +16,10 @@ export default class Editbar extends Component {
     if(this.state.shown){
       return (
         <div className="Editbar" onClick={this.toggle}>
-          {this.displayItems()}
-          {this.displayToolbar()}
+          <Itembar
+            items={this.props.items}
+          />
+          <Toolbar />
         </div>
       )}
     else {
@@ -28,28 +30,18 @@ export default class Editbar extends Component {
         >
         </div>
       )
-      }
+    }
   }
 
 ///////////////////////////////////////
 
   toggle = (event) => {
-    if(!(event.target.attributes.getNamedItem("src"))){
-    this.setState({shown: !this.state.shown})}
+    // console.log(event.target.attributes)
+    // if(!(event.target.attributes.getNamedItem("src"))){
+      this.setState({
+        shown: !this.state.shown
+      })
+    // }
   }
 
-  displayItems = () => {
-    return this.props.items.map(item => {
-      return (
-        <EditbarItem key={1000+item.id} item={item} />
-      )
-    })
-  }
-
-  displayToolbar = () => {
-    return (
-      // <Toolbar />
-      null
-    )
-  }
 }
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../assets/css/Toolbar.css'
 
-const Toolbar = (props) => {
-  return(
-    <div className="Toolbar">
-      tools
-    </div>
-  )
+class Toolbar extends Component {
+  render() {
+    return(
+      <div className="Toolbar">
+        tools
+      </div>
+    )
+  }
 }
 
 export default Toolbar
