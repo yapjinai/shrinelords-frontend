ac84c16eb6d0aabdf70ac8f1885c80109d5364f3
yapjinai <jinai.jyap@gmail.com>
2018-11-12 17:33:35 -0500

    changed menu effect from javascript to CSS

--- src/components/Editbar.js
+++ src/components/Editbar.js
@@ -4,44 +4,17 @@ import Itembar from './Itembar'
 import Toolbar from './Toolbar'
 
 export default class Editbar extends Component {
-
-  constructor() {
-    super()
-    this.state = {
-      shown: true
-    }
-  }
-
   render() {
-    if(this.state.shown){
-      return (
-        <div className="Editbar" onClick={this.toggle}>
-          <Itembar
-            items={this.props.items}
-          />
-          <Toolbar />
-        </div>
-      )}
-    else {
-      return (
-        <div
-          className="Editbar Editbar-hidden"
-          onClick={this.toggle}
-        >
-        </div>
-      )
-    }
+    return (
+      <div className="Editbar" onClick={this.toggle}>
+        <Itembar
+          items={this.props.items}
+        />
+        <Toolbar />
+      </div>
+    )
   }
 
 ///////////////////////////////////////
-
-  toggle = (event) => {
-    // console.log(event.target.attributes)
-    // if(!(event.target.attributes.getNamedItem("src"))){
-      this.setState({
-        shown: !this.state.shown
-      })
-    // }
-  }
 
 }
