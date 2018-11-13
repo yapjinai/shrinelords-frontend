0e7fedd0bc245c18d8a939c29a6865025fe31663
yapjinai <jinai.jyap@gmail.com>
2018-11-13 18:21:30 -0500

    working on keyboard shortcuts

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -12,46 +12,66 @@ class Toolbar extends Component {
     )
   }
 
+  componentDidMount() {
+    console.log('hi');
+    document.addEventListener('keyDown', this.handleKeyDown)
+  }
+
   //////////////////////////
 
   displayMouseModeButtons = () => {
-    const mouseModes = [
-      'move',
-      'delete',
-      'up',
-      'down',
-      'top',
-      'bottom',
-    ]
     // const mouseModes = [
-    //   {
-    //     mode: 'select',
-    //     buttonName: 'Select'
-    //   },
-    //   {
-    //     mode: 'delete',
-    //     buttonName: 'Delete'
-    //   },
-    //   {
-    //     mode: 'up',
-    //     buttonName: 'Move up'
-    //   },
-    //   {
-    //     mode: 'down',
-    //     buttonName: 'Move down'
-    //   }
+    //   'move',
+    //   'delete',
+    //   'up',
+    //   'down',
+    //   'top',
+    //   'bottom',
     // ]
+    const mouseModes = [
+      {
+        mode: 'move',
+        buttonName: 'Move'
+      },
+      {
+        mode: 'delete',
+        buttonName: 'Delete'
+      },
+      {
+        mode: 'up',
+        buttonName: 'Move up'
+      },
+      {
+        mode: 'down',
+        buttonName: 'Move down'
+      },
+      {
+        mode: 'top',
+        buttonName: 'Move to top'
+      },
+      {
+        mode: 'bottom',
+        buttonName: 'Move to bottom'
+      },
+    ]
     return mouseModes.map(m => {
       return (
         <li
-          key={m}
+          key={m.mode}
         >
           <img
-            src={`../../assets/img/${m}.png`}
-            name={m}
-            alt={m}
+            src={`../../assets/img/${m.mode}.png`}
+            name={m.mode}
+            alt={m.buttonName}
             onClick={this.handleClick}
+            onMouseOver={this.handleMouseOver}
+            onMouseOut={this.handleMouseOut}
           />
+          <div
+            className={`info`}
+          >
+            {m.buttonName}
+          </div>
         </li>
       )
     })
@@ -60,6 +80,27 @@ class Toolbar extends Component {
   handleClick = (e) => {
     e.preventDefault()
     this.props.updateMouseMode(e.target.name)
+  }
+
+  handleKeyDown = (e) => {
+    console.log(e.key);
+    switch (e.key) {
+      case 'm':
+        console.log('hi');
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleMouseOver = (e) => {
+    const infoDiv = e.target.nextSibling
+    infoDiv.classList.add('display')
+  }
+
+  handleMouseOut = (e) => {
+    const infoDiv = e.target.nextSibling
+    infoDiv.classList.remove('display')
   }
 }
 
