456974774ca5cbd77297d45d3f90f30018eac35e
Chris Stull <chris.stull@gmail.com>
2018-11-13 18:11:32 -0500

    Links from shrine back to shrines and about page.

---
+++ src/containers/About.js
@@ -0,0 +1,13 @@
+import React, { Component,PureComponent } from 'react'
+
+export default class About extends Component {
+
+  render(){
+    return(
+      <div>
+        <p>This is the about page</p>
+      </div>
+    )
+  }
+
+}
