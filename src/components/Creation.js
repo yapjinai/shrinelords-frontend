f3f8e63c53038c5202e9cecde9bb34d4f81a2b9c
Chris Stull <chris.stull@gmail.com>
2018-11-14 18:17:52 -0500

    Allow creation of shrines and redirection to main shrines page

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -3,6 +3,7 @@ import Background from './Background'
 import '../assets/css/Creation.css'
 
 const backsURL = "http://localhost:3000/api/v1/backs"
+const shrinesURL = "http://localhost:3000/api/v1/shrines"
 
 export default class Creation extends Component {
 
@@ -42,7 +43,24 @@ export default class Creation extends Component {
     this.setState({back_index: newindex}, load_and_play())
   }
 
-
+  newShrine = () => {
+    let shrineinput = document.getElementById('shrineinput')
+    let back_id = this.state.backs[this.state.back_index].id
+    if(!!shrineinput.value && !(shrineinput.value.replace(/\s/,"")==="")){
+      fetch(shrinesURL,{
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          name: shrineinput.value,
+          back_id: back_id
+        })
+      })
+      .then(res => window.location="http://localhost:3001")
+    }
+  }
 
   background = (back) => {
     return (
@@ -67,9 +85,10 @@ export default class Creation extends Component {
       <div>
         <center>
           <p className="creationtext">Create a shrine...</p>
-          <input type='text' placeholder='shrine name'></input><br></br>
+          <input id='shrineinput' type='text' placeholder='shrine name'></input><br></br>
           <button className="setting" onClick={this.previousback}>Previous shrine setting</button>
-          <button className="setting" onClick={this.nextback}>Next shrine setting</button>
+          <button className="setting" onClick={this.nextback}>Next shrine setting</button><br></br>
+          <button className="setting" onClick={this.newShrine}>Create!</button>
         </center>
         {this.state.backs.length ? this.background(this.state.backs[this.state.back_index].video) : null}
       </div>
