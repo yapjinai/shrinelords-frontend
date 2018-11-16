0eeab4d5784a780e53ba5359c1d3d792fc153318
Chris Stull <chris.stull@gmail.com>
2018-11-16 10:59:57 -0500

    About page done.

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import '../assets/css/About.css'
+import { Link } from 'react-router-dom'
 
 export default class About extends Component {
 
   render(){
     return(
       <div className="about">
-        <img src="https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2015/04/fushimi-inari-night.jpg" alt="" className="aboutshrine"></img>
+        <Link className="aboutlink" to="/">
+          <img src="https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2015/04/fushimi-inari-night.jpg" alt="" className="aboutshrine"></img>
+          <h4>A Shrine is a place of peace, a place to offer precious things.<br></br>Would you, too, like to be a Shrinelord?</h4></Link>
       </div>
     )
   }
