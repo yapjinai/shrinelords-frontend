ef71538cc04e585e66be2d28109eaba7dfd4ae4d
Chris Stull <chris.stull@gmail.com>
2018-11-16 10:39:24 -0500

    About shrine scaled

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import '../assets/css/About.css'
 
 export default class About extends Component {
 
   render(){
     return(
-      <div>
-        <p>This is the about page</p>
+      <div className="about">
+        <img src="https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2015/04/fushimi-inari-night.jpg" alt="" className="aboutshrine"></img>
       </div>
     )
   }
